04815e83fae9ccdd7548542eca7a1049d5e56a8d
Punith <144771860+Puneeetth@users.noreply.github.com>
2026-02-17 20:07:39 +0530

    entities

--- src/main/java/com/scribble/model/User.java
+++ src/main/java/com/scribble/model/User.java
@@ -1,8 +1,39 @@
 package com.scribble.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true,length = 50)
+    private String userName;
+    @Column(nullable = false)
+    private String totalScore;
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
 
 }
