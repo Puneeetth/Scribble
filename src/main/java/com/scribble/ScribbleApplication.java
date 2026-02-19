9d4acd5421d6bbd75ff2e9eefc42d4a46ecbd3df
Punith <144771860+Puneeetth@users.noreply.github.com>
2026-02-19 12:28:35 +0530

    updated

--- src/main/java/com/scribble/ScribbleApplication.java
+++ src/main/java/com/scribble/ScribbleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScribbleApplication {
-
+  //updated
 	public static void main(String[] args) {
 		SpringApplication.run(ScribbleApplication.class, args);
 	}
